7e26de9df37384cd68c8427c9e445f641f1abfaa
Jorge Villasmil <keine1lust@gmail.com>
2020-11-30 14:58:26 -0400

    make it better

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'db4free.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'test_jor'),
+            'username' => env('DB_USERNAME', 'test_jor'),
+            'password' => env('DB_PASSWORD', 'test_jor'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8',
             'collation' => 'utf8_general_ci',
